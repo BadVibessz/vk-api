820d1b1e42b5673e7e140fec81517b5a73503ee6
Oskar <danila.tsymbal@mail.ru>
2023-10-19 23:04:47 +0300

    todo: req fails

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	http "net/http"
-	"vk-api"
+	"os"
+	"strconv"
+	vkapi "vk-api"
 )
 
 //func ContextClient(ctx context.Context) *http.Client {
@@ -19,21 +25,55 @@ import (
 //	return http.DefaultClient
 //}
 
+func PrettyString(str string) (string, error) {
+	var prettyJSON bytes.Buffer
+	if err := json.Indent(&prettyJSON, []byte(str), "", "    "); err != nil {
+		return "", err
+	}
+	return prettyJSON.String(), nil
+}
+
 func main() {
+
+	logger := log.New(os.Stderr, "", 3)
 
 	h := http.Client{}
 	client := vkapi.Client{
 		Http:       &h,
-		BaseURL:    "http://google.com",
+		BaseURL:    "https://api.vk.com/method/",
 		Retry:      false,
 		RetryCount: 0}
 
 	ctx := context.Background()
 
-	resp, err := client.Post(ctx, "", nil)
-	if err != nil {
-		fmt.Errorf(err.Error())
+	//resp, err := client.Post(ctx, "", nil, nil)
+	//if err != nil {
+	//	logger.Println(err)
+	//}
+
+	vk := vkapi.VkAPI{
+		Token:   "...",
+		Version: "5.154",
+		Client:  &client,
 	}
 
-	println(resp)
+	resp, err := vk.SendMessage(ctx, vkapi.Params{
+		"message":   "=)",
+		"random_id": strconv.Itoa(0),
+		"peer_id":   strconv.Itoa(2000000000 + 1),
+	})
+	if err != nil {
+		logger.Println(err)
+	}
+
+	//resp, err := h.Get("http://www.yandex.ru")
+	//if err != nil {
+	//	logger.Println(err)
+	//}
+
+	buf, err := io.ReadAll(resp.Body)
+	if err != nil {
+		logger.Println(err)
+	}
+	fmt.Println(PrettyString(string(buf)))
 }
--- vk.go
+++ vk.go
@@ -20,20 +20,98 @@ type Client struct {
 	RetryCount int  // what times should we retry
 }
 
-type Body map[string]string
+type Params map[string]string
 
-func (c *Client) Post(ctx context.Context, url string, body Body) (*http.Response, error) {
+func (c *Client) Post(ctx context.Context, url string, queryParams, body Params) (*http.Response, error) {
 
-	data, err := json.Marshal(body)
+	var req *http.Request
+	var err error
+
+	if body != nil {
+		data, err := json.Marshal(body)
+		if err != nil {
+			return nil, err
+		}
+		reader := bytes.NewReader(data)
+
+		req, err = http.NewRequestWithContext(ctx, "POST", c.BaseURL+url, reader)
+		if err != nil {
+			return nil, err
+		}
+
+	} else {
+		req, err = http.NewRequestWithContext(ctx, "POST", c.BaseURL+url, nil)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if queryParams != nil {
+		q := req.URL.Query()
+		for k, v := range queryParams {
+			q.Add(k, v)
+		}
+		req.URL.RawQuery = q.Encode()
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-encoded")
+
+	resp, err := c.Http.Do(req)
+
 	if err != nil {
 		return nil, err
 	}
 
-	reader := bytes.NewReader(data)
+	return resp, nil
+}
 
-	req, err := http.NewRequestWithContext(ctx, "POST", url, reader)
+func (c *Client) Get(ctx context.Context, url string, queryParams Params) (*http.Response, error) {
+
+	req, err := http.NewRequestWithContext(ctx, "GET", c.BaseURL+url, nil)
+
+	if queryParams != nil {
+		q := req.URL.Query()
+		for k, v := range queryParams {
+			q.Add(k, v)
+		}
+		req.URL.RawQuery = q.Encode()
+	}
 
 	resp, err := c.Http.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+type VkAPI struct {
+	Client  *Client
+	Token   string
+	Version string
+}
+
+// todo: return VkResponse
+func (vk *VkAPI) call(ctx context.Context, method string, body Params) (*http.Response, error) {
+
+	queryParams := make(Params)
+	queryParams["access_token"] = vk.Token
+	queryParams["v"] = vk.Version
+	// queryParams["oauth"] = vk.Version
+
+	resp, err := vk.Client.Post(ctx, method, queryParams, body)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+// todo: return VkResponse
+// SendMessage https://dev.vk.com/ru/method/messages.send
+func (vk *VkAPI) SendMessage(ctx context.Context, body Params) (*http.Response, error) {
+
+	resp, err := vk.call(ctx, "messages.send/", body)
 	if err != nil {
 		return nil, err
 	}
